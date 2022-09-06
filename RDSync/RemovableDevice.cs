777256d7eaa72f212c0c31347371849d17111224
brice <brice@BAZOOKA>
2022-09-06 22:30:52 +0200

    Ajoutez des fichiers projet.

---
+++ RDSync/RemovableDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDSync
+{
+
+    /// <summary>
+    /// Represents a removable device (media device)
+    /// </summary>
+    internal class RemovableDevice
+    {
+        /// <summary>
+        /// This Id must persist between sessions
+        /// </summary>
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Details { get; set; }
+
+        public IEnumerable<string> ListFiles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
